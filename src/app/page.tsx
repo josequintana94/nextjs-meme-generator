f77b6fe1fa6c8a93a3997bd047853011ed91273e
Webster Lima <websterhlima@gmail.com>
2023-05-16 12:32:37 -0300

    MemeDisplay with dynamic size and new data structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,28 @@ import MemeDisplay from "./(components)/MemeDisplay";
 export default function Home() {
   return (
     <main className="max-w-[1200px] mx-auto">
-      <MemeDisplay
-        background={{
-          src: "/disaster-girl.jpg",
-          width: 1200,
-          height: 900,
-          alt: "Disaster Girl",
-        }}
-        textareas={[
-          {
-            id: "saying",
-            top: 750,
-            left: 100,
-            width: 1000,
-            height: 100,
-            fontSize: 100,
-            color: "white",
-            text: "I'm gonna be rich!",
-          },
-        ]}
-      />
+      <div className="max-w-[300px]">
+        <MemeDisplay
+          background={{
+            src: "/disaster-girl.jpg",
+            width: 1200,
+            height: 900,
+            alt: "Disaster Girl",
+          }}
+          textareas={[
+            {
+              id: "saying",
+              top: 750,
+              left: 100,
+              width: 1000,
+              height: 100,
+              fontSize: 100,
+              color: "white",
+              text: "I'm gonna be rich!",
+            },
+          ]}
+        />
+      </div>
     </main>
   );
 }
